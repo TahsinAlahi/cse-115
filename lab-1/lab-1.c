f676039e34fb76cb83ec20342017039020cfa4a0
Tahsin <alahitahsin@gmail.com>
2025-02-03 19:26:43 +0600

    lab-1

---
+++ lab-1/lab-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+printf("NUMBER\tSQUARE\tCUBE\n");
+printf("******\t******\t****\n\n");
+for(int i=1;i<10;i++){
+    printf("%d\t",i);
+    printf("%d\t",i*i);
+    printf("%d\n",i*i*i);
+
+}
+}
---
+++ lab-1/lab-1_house.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+printf("______________\n");
+printf("//////|\\\\\\\\\\\\\\\\ \n");
+printf("---------------\n");
+printf("|             |\n");
+printf("|     ___     |\n");
+printf("| | | | | | | |\n");
+printf("|     | |     | \n");
+printf("|_____________|\n");
+}
